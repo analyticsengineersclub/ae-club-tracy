d615a6bb4ad29d7400bf5d2675f37157df1e5a03
tracy-u <tracy.allen.urquhart@gmail.com>
2021-12-14 18:54:58 +0000

    adding follow-along unit test lecture

--- aec-python/calc.py
+++ aec-python/calc.py
@@ -20,39 +20,44 @@ divide.add_argument("integers_to_divide", nargs='*', type=int)
 multiply = subparsers.add_parser("multiply", help="multiply integers")
 multiply.add_argument("integers_to_multiply", nargs='*', type=int)
 
-args = parser.parse_args()
-
-if args.command == "add":
-    sum = sum(args.integers_to_sum)
-    if len(args.integers_to_sum) == 1: 
-        print(f"Only one integer was passed; the value is {sum}")
-    else:
-        print(f"The sum of these values is {sum}")
-
-if args.command == "subtract":
-    difference = args.integers_to_subtract[0]
-    if len(args.integers_to_subtract) == 1: 
+def aec_subtract(integers_to_subtract):
+    difference = integers_to_subtract[0]
+    if len(integers_to_subtract) == 1: 
         print(f"Only one integer was passed; the value is {difference}")
-    for i in range(1, len(args.integers_to_subtract)):
-        difference -= args.integers_to_subtract[i]
+    for i in range(1, len(integers_to_subtract)):
+        difference -= integers_to_subtract[i]
     print(f"The difference of these values is {difference}")
+    return difference
 
-if args.command == "multiply":
-    product = args.integers_to_multiply[0]
-    if len(args.integers_to_multiply) == 1: 
-        print(f"Only one integer was passed; the value is {product}")
-    else:
-        for i in range(1, len(args.integers_to_multiply)):
-            product *= args.integers_to_multiply[i]
-        print(f"The product of these values is {product}")
+if __name__ == "__main__":
+    args = parser.parse_args()
 
-if args.command == "divide":
-    quotient = args.integers_to_divide[0] 
-    if len(args.integers_to_divide) == 1: 
-        print(f"Only one integer was passed; the value is {quotient}")
-    elif 0 in args.integers_to_divide[1:]:
-        print("Whoops! Cannot divide by 0!")
-    else: 
-        for i in range(1, len(args.integers_to_divide)):
-            quotient /= args.integers_to_divide[i]
-        print(f"The quotient of these values is {quotient}")
+    if args.command == "add":
+        sum = sum(args.integers_to_sum)
+        if len(args.integers_to_sum) == 1: 
+            print(f"Only one integer was passed; the value is {sum}")
+        else:
+            print(f"The sum of these values is {sum}")
+
+    if args.command == "subtract":
+        aec_subtract(args.integers_to_subtract)
+
+    if args.command == "multiply":
+        product = args.integers_to_multiply[0]
+        if len(args.integers_to_multiply) == 1: 
+            print(f"Only one integer was passed; the value is {product}")
+        else:
+            for i in range(1, len(args.integers_to_multiply)):
+                product *= args.integers_to_multiply[i]
+            print(f"The product of these values is {product}")
+
+    if args.command == "divide":
+        quotient = args.integers_to_divide[0] 
+        if len(args.integers_to_divide) == 1: 
+            print(f"Only one integer was passed; the value is {quotient}")
+        elif 0 in args.integers_to_divide[1:]:
+            print("Whoops! Cannot divide by 0!")
+        else: 
+            for i in range(1, len(args.integers_to_divide)):
+                quotient /= args.integers_to_divide[i]
+            print(f"The quotient of these values is {quotient}")
