95c2f4d65663338829ebe03c391e3da31a78e0a1
tracy-u <tracy.allen.urquhart@gmail.com>
2021-12-14 17:29:47 +0000

    add conditional for one value

--- aec-python/calc.py
+++ aec-python/calc.py
@@ -1,24 +1,58 @@
 # calc.py 
-
 import argparse
 
 parser = argparse.ArgumentParser(description = "CLI Calculator")
-
 subparsers = parser.add_subparsers(help="sub-command help", dest="command")
+
 # sum subparser
 add = subparsers.add_parser("add", help="add integers")
-add.add_argument("ints_to_sum", nargs=2, type=int)
+add.add_argument("integers_to_sum", nargs='*', type=int)
 
 # difference subparser
-sub = subparsers.add_parser("sub", help="subtract integers")
-sub.add_argument("ints_to_sub", nargs=2, type=int)
+subtract = subparsers.add_parser("subtract", help="subtract integers")
+subtract.add_argument("integers_to_subtract", nargs='*', type=int)
+
+# quotient subparser
+divide = subparsers.add_parser("divide", help="divide integers")
+divide.add_argument("integers_to_divide", nargs='*', type=int)
+
+# product subparser
+multiply = subparsers.add_parser("multiply", help="multiply integers")
+multiply.add_argument("integers_to_multiply", nargs='*', type=int)
 
 args = parser.parse_args()
 
 if args.command == "add":
-    our_sum = sum(args.ints_to_sum)
-    print(f"the sum of values is {our_sum}")
+    sum = sum(args.integers_to_sum)
+    if len(args.integers_to_sum) == 1: 
+        print(f"Only one integer was passed; the value is {sum}")
+    else:
+        print(f"The sum of these values is {sum}")
 
-if args.command == "sub":
-    our_sub = args.ints_to_sub[0] - args.ints_to_sub[1]
-    print(f"the difference of values is {our_sub}")
+if args.command == "subtract":
+    difference = args.integers_to_subtract[0]
+    if len(args.integers_to_subtract) == 1: 
+        print(f"Only one integer was passed; the value is {difference}")
+    for i in range(1, len(args.integers_to_subtract)):
+        difference -= args.integers_to_subtract[i]
+    print(f"The difference of these values is {difference}")
+
+if args.command == "multiply":
+    product = args.integers_to_multiply[0]
+    if len(args.integers_to_multiply) == 1: 
+        print(f"Only one integer was passed; the value is {product}")
+    else:
+        for i in range(1, len(args.integers_to_multiply)):
+            product *= args.integers_to_multiply[i]
+        print(f"The product of these values is {product}")
+
+if args.command == "divide":
+    quotient = args.integers_to_divide[0] 
+    if len(args.integers_to_divide) == 1: 
+        print(f"Only one integer was passed; the value is {quotient}")
+    elif 0 in args.integers_to_divide[1:]:
+        print("Whoops! Cannot divide by 0!")
+    else: 
+        for i in range(1, len(args.integers_to_divide)):
+            quotient /= args.integers_to_divide[i]
+        print(f"The quotient of these values is {quotient}")
