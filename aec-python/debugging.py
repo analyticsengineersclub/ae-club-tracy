c1fd7170df0d9952c79445bbcd2b8b2e5f49e0b8
tracy-u <tracy.allen.urquhart@gmail.com>
2021-12-16 21:42:13 +0000

    additional clean up

--- aec-python/debugging.py
+++ aec-python/debugging.py
@@ -1,4 +1,13 @@
+# debugging exercise
 
+# print
+for i in range(10):
+	print(i)
+	x = denoms[i]
+	print(x)
+	result = 100 / x
+
+# using pdb
 for i in range(10):
 	import pdb; pdb.set_trace()
 	print(f'i: {i}')
